34af70d0f0fab2f6f98a054532ccbac5d94355e0
santhoshi17-29 <santhoshi2908@gmail.com>
2024-12-31 11:41:01 +0530

    Initial commit 22

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -56,7 +56,7 @@
 
 
 function cd(){
-    var countDownDate = new Date("Feb 13, 2024 00:00:00 ").getTime();
+    var countDownDate = new Date("Jan 24, 2025 00:00:00 ").getTime();
 var x= setInterval(function(){
    var now = new Date().getTime();
     var distance= countDownDate - now;
